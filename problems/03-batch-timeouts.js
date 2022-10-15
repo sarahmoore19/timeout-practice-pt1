578b43c13ac5cfa6bb5f1e10c192db5860d50c09
sarahmoore <sarahnmoore19@gmail.com>
2022-10-15 10:23:21 -0400

    solved problems

--- problems/03-batch-timeouts.js
+++ problems/03-batch-timeouts.js
@@ -5,17 +5,12 @@ callback in the array with its corresponding delay. For example, the
 callback at index 0 should be set with the delay at index 0,
 the callback at index 1 should be set with the delay at index 1, and so on.
 The `batchTimeouts` function should return an array containing the Timeout
-objects for each timeout that was set. You may assume that both array arguments 
+objects for each timeout that was set. You may assume that both array arguments
 have the same length.
 
-In addition to Mocha, we recommend that you test your code manually using 
-node with the examples below to confirm the correct behavior.
-
 Note: The test specs for this problem are valid for iterative solutions. If you
-pass the specs for an iterative solution and then attempt a recursive solution,
+pass the specs for an iterative solcution and then attempt a reursive solution,
 you will need to test your new solution manually using node.
-
-Example:
 
 const sayHello = () => console.log('hi');
 const sayGoodbye = () => console.log('bye');
@@ -23,8 +18,8 @@ const shout = () => console.log('WHAT?');
 const tasks = [sayHello, sayGoodbye, shout];
 const delays = [500, 200, 900];
 
-const timeoutObjs = batchTimeouts(tasks, delays); 
-// should print: 
+const timeoutObjs = batchTimeouts(tasks, delays);
+// should print:
 //  'bye' after 200 ms
 //  'hi' after 500 ms
 //  'WHAT?' after 900 ms
@@ -33,8 +28,21 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(callbacks, delays) {
-  // Your code here
+  let newArr = [];
+   for (let i = 0; i < callbacks.length; i++) {
+    newArr.push(setTimeout(callbacks[i], delays[i]));
+  }
+  return newArr;
 }
+
+/* function batchTimeouts(callbacks, delays, newArr = []) {
+  if (callbacks.length > 0) {
+    newArr.push(setTimeout(callbacks[0], delays[0]));
+    batchTimeouts(callbacks.slice(1), delays.slice(1), newArr)
+  }
+  else console.log(newArr);
+} */
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
